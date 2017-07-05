60d548c1eede5a2ebb908cd3c04d7e367b7aa422
李二狗 <chanli1@creditease.cn>
2017-07-05 15:57:48 +0800

    [add] 初始化项目

---
+++ DMAppConstructionDemo/DMAppConstructionDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DMAppConstructionDemo
+//
+//  Created by 李二狗 on 2017/7/5.
+//  Copyright © 2017年 YRHY Science and Technology (Beijing) Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
